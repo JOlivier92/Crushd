216e551c433cf9bb0015bd050e4f5d171424277f
Joseph Olivier <josepholivier92@gmail.com>
2018-10-20 23:44:22 -0700

    Fix unassigned variable

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,33 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jsonwebtoken = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // route for signing up.
 router.post('/register', (req, res) => {
     User.findOne({username: req.body.username}).then(user => {
+        // if the user already exists in the database, return 400 level error
         if (user) {
+            let errors = {};
             errors.name = 'User already exists';
             return res.status(400).json(errors)
         } else {
-            let newUser = new User({
+        // else, assign newUser to posted body data
+            const newUser = new User({
                 username: req.body.username,
                 email: req.body.email,
                 phone_number: req.body.phone_number,
                 birthdate: req.body.birthdate,
-                password: req.body.password,
+                password: req.body.password
             })
+            // generate salt with 10 iterations, pass errors / generated salt to CB
             bcrypt.genSalt(10, (err, salt) => {
+                // attach salt to given password, pass errors / new hash to CB
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
                     if(err) throw err;
+                    // if no errors, save the user to the database with generated hash
                     newUser.password = hash;
+                    // if user successfully saves, pass payload as response to server
                     newUser.save()
-                        .then(user => {
-                            const payload = {id: user.id, name: user.username};
-                            res.json({
-                                success: true
-                            })
-                        })
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
                 })
             })
         }
