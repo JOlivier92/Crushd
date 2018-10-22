fb4a22cdb42ac2b51ccc93657c461511da75683b
sirivat <dsirivat17@gmail.com>
2018-10-21 17:11:10 -0700

    Implement front end auth

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Provider } from "react-redux";
+import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
+
+import GreetingContainer from "./components/greeting/greeting_container";
+import SignUpFormContainer from "./components/session_form/signup_form_container";
+import LogInFormContainer from "./components/session_form/login_form_container";
+import { AuthRoute, ProtectedRoute } from "./util/route_util";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Crush'd</h1>
+          <GreetingContainer />
+        </header>
+
+        <Switch>
+          <AuthRoute exact path="/login" component={LogInFormContainer} />
+          <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import jwt_decode from "jwt-decode";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as APIUtil from "./util/session_api_util";
+import configureStore from "./store/store";
+import Root from "./components/root";
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+document.addEventListener("DOMContentLoaded", () => {
+  let store = configureStore();
+  // Check for token
+  if (localStorage.jwtToken) {
+    // Set auth token header auth
+    APIUtil.setAuthToken(localStorage.jwtToken);
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(localStorage.jwtToken);
+    // Set user and isAuthenticated
+    store.dispatch(APIUtil.setCurrentUser(decoded));
+
+    // Check for expired token
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+      // Logout user
+      store.dispatch(APIUtil.logoutUser());
+      // Redirect to login
+      window.location.href = "/login";
+    }
+  }
+  const root = document.getElementById("root");
+  ReactDOM.render(<Root store={store} />, root);
+  serviceWorker.register();
+});
