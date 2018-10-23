d020ec9c69bd13754cf4d9249927a3905d7ec563
Joseph Olivier <josepholivier92@gmail.com>
2018-10-23 09:59:05 -0700

    try to fix heroku issue

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 // Imports section
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+
 
 const express = require('express');
 const app = express();
 
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const dbURI = require('./config/keys');
