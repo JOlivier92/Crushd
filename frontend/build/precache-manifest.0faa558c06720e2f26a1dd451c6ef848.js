680b2c38f93e9e11579342154bc79a338f5ab9ab
Joseph Olivier <josepholivier92@gmail.com>
2018-10-23 11:54:10 -0700

    try to fix heroku issue

---
+++ frontend/build/precache-manifest.0faa558c06720e2f26a1dd451c6ef848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3a9eeb7cb2bbae52534",
+    "url": "/static/js/main.c3a9eeb7.chunk.js"
+  },
+  {
+    "revision": "a08e3db9d8bc2b8b0c43",
+    "url": "/static/js/1.a08e3db9.chunk.js"
+  },
+  {
+    "revision": "c3a9eeb7cb2bbae52534",
+    "url": "/static/css/main.b31d2dea.chunk.css"
+  },
+  {
+    "revision": "d8456afe22ecf1e84edcbbf6d52b87c7",
+    "url": "/index.html"
+  }
+];
