1b69e6e52c239271e3bc9de5bfb0048bdd5d2479
domobritton <domonicbritton@gmail.com>
2018-10-22 20:04:38 -0700

    Add modals

---
+++ frontend/src/components/modal/modal.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { connect } from "react-redux";
+import { closeModal } from "../../actions/modal_actions";
+import LoginFormContainer from "../session_form/login_form_container";
+import SignupFormContainer from "../session_form/signup_form_container";
+
+const Modal = props => {
+  if (!props.modal) {
+    return null;
+  }
+
+  let component;
+
+  switch (props.modal.modal) {
+    case "ShowSignup":
+      component = <SignupFormContainer />;
+      break;
+    case "ShowLogin":
+      component = <LoginFormContainer />;
+      break;
+    default:
+      return null;
+  }
+
+  return (
+    <div className="modal-background">
+      <div className="modal-child" onClick={e => e.stopPropagation()}>
+        {component}
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    modal: state.ui.modal
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    closeModal: () => dispatch(closeModal())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
---
+++ frontend/src/reducers/modal_reducer.js
@@ -0,0 +1,21 @@
+import {
+    OPEN_MODAL,
+    CLOSE_MODAL
+} from '../actions/modal_actions';
+
+const modalsReducer = (state = {}, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case OPEN_MODAL:
+            return {
+                modal: action.modal,
+                pin: action.pin
+            };
+        case CLOSE_MODAL:
+            return null;
+        default:
+            return state;
+    }
+};
+
+export default modalsReducer;
