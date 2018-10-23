f176e2e7a3476f23667c84f3b008efde6dc23d0c
Joseph Olivier <josepholivier92@gmail.com>
2018-10-23 10:55:31 -0700

    attempt to fix heroku issue only showing backend

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use('/api/users/login', users.login);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
-app.use('/frontend', express.static("public"));
+app.use(express.static("frontend"));
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/frontend/public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
