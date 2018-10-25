a51a14e391a8591f6688b691a43d0e7ba0442caa
Joseph Olivier <josepholivier92@gmail.com>
2018-10-25 10:53:33 -0700

    Add heart loader to homepage. add asynchronous wait function. change README

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import "./home.css";
+import Loader from "react-loader-spinner";
 
 const videoType = "video/webm";
 
@@ -13,20 +14,28 @@ class Home extends React.Component {
     super(props);
     this.state = {
       recording: false,
-      videos: []
+      videos: [],
+      loading: true
     };
     this.uploadVideo = this.uploadVideo.bind(this);
   }
 
   async componentDidMount() {
+    this.setState({loading: true})
+    
     const stream = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: true
     });
+    
+    console.log(this.state);
 
     // Show video recorder to user
-    this.video.src = window.URL.createObjectURL(stream);
-    this.video.play();
+    if (!this.state.loading) {
+      this.video.src = window.URL.createObjectURL(stream);
+      this.video.play();
+    }
+    
 
     //Initialize recording
     this.mediaRecorder = new MediaRecorder(stream, {
@@ -42,6 +51,12 @@ class Home extends React.Component {
         this.chunks.push(e.data);
       }
     };
+    await this.sleep(1500);
+    this.setState({ loading: false}); 
+  }
+
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   startRecording(e) {
@@ -129,7 +144,14 @@ class Home extends React.Component {
   }
 
   render() {
-    const { recording, videos } = this.state;
+    const { recording, videos, loading } = this.state;
+    if (this.state.loading) {
+      return (
+        <div className="loader-container">
+          <Loader className="spinner" type="Hearts" height="250" width="250" />;
+        </div>
+      )  
+    }
     return (
       <div className="home-content-section">
         <div className="camera">
