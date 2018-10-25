a4d121b0f713225d9e925c592bb8f444026abe8d
domobritton <domonicbritton@gmail.com>
2018-10-24 17:33:42 -0700

    Add changes to signup form

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { Link } from "react-router-dom";
 import "./splash.css";
 
-const Splash = () => (
-
-    <div>
-        <div className="splash-container"></div>
-        
-    
-    </div>
-
-);
-
+class Splash extends Component {
+  render() {
+    return <div>
+        <div className="splash-container">
+          <Carousel showArrows={false} autoPlay showIndicators={false} showThumbs={false} infiniteLoop={true} stopOnHover={true} transitionTime={950} showStatus={false} interval={5000}>
+            <div>
+              <img src="https://bit.ly/2SeV0YP" />
+              <p className="legend">Legend 1</p>
+            </div>
+            <div>
+              <img src="https://bit.ly/2D3xpFU" />
+              <p className="legend">Legend 2</p>
+            </div>
+            <div>
+              <img src="https://bit.ly/2Sh3fUi" />
+              <p className="legend">Legend 3</p>
+            </div>
+            <div>
+              <img src="https://bit.ly/2SeV0YP" />
+              <p className="legend">Legend 1</p>
+            </div>
+            <div>
+              <img src="https://bit.ly/2D3xpFU" />
+              <p className="legend">Legend 2</p>
+            </div>
+            <div>
+              <img src="https://bit.ly/2Sh3fUi" />
+              <p className="legend">Legend 3</p>
+            </div>
+            <div>
+              <img src="https://bit.ly/2SeV0YP" />
+              <p className="legend">Legend 1</p>
+            </div>
+            <div>
+              <img src="https://bit.ly/2D3xpFU" />
+              <p className="legend">Legend 2</p>
+            </div>
+            <div>
+              <img src="https://bit.ly/2Sh3fUi" />
+              <p className="legend">Legend 3</p>
+            </div>
+          </Carousel>
+        </div>
+        <div className="mobile-splash-container">
+ </div>
+      </div>;
+  }
+}
 
 export default Splash;
-
-
-
-
