75af74b350de829d3aaac875b6197c9188d8937b
sirivat <dsirivat17@gmail.com>
2018-10-25 16:46:30 -0700

    Save video to mongo

--- models/Video.js
+++ models/Video.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //Define a Schema
 const Schema = mongoose.Schema;
 
 const VideoSchema = new Schema({
-    user_id: {
-        type: Number,
-        required: true
-    },
+  user_id: {
+    type: String,
+    required: true
+  },
 
-    // URL to video 
-    videoURL: {
-        type: String,
-        required: true
-    },
-    // array if video IDs
-    // that have responded to
-    // the current video
-})
+  // URL to video
+  videoURL: {
+    type: String,
+    required: true
+  }
+  // array if video IDs
+  // that have responded to
+  // the current video
+});
 
-module.exports = Video = mongoose.model('videos', VideoSchema);
+module.exports = Video = mongoose.model("videos", VideoSchema);
