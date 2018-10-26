e5a94d2ccb7835f8cf07a2cdf26e910b934c1b2f
sirivat <dsirivat17@gmail.com>
2018-10-26 10:43:18 -0700

    Add upload-option-section

--- frontend/src/components/responses/responses_index.jsx
+++ frontend/src/components/responses/responses_index.jsx
@@ -3,46 +3,29 @@ import "./responses_index.css";
 
 class ResponsesIndex extends React.Component {
   render() {
-    return <div className="responses-index-view">
+    const uploadVideo = () => (
+      <div className="upload-video-option-container">
+        <h3>No responses yet...</h3>
+        <button>Upload a new video</button>
+      </div>
+    );
+    return (
+      <div className="responses-index-view">
         <div className="response-outer">
           <div className="response-inner">
             <div className="image-thumbnail">
-              <img className="profile-img" src="https://bit.ly/2Pmd5FK" alt="profile-thumb" />
+              <img
+                className="profile-img"
+                src="https://bit.ly/2Pmd5FK"
+                alt="profile-thumb"
+              />
               <p>Andrea</p>
             </div>
           </div>
-          <div className="response-inner">
-            <div className="image-thumbnail">
-              <img className="profile-img" src="https://bit.ly/2Pmd5FK" alt="profile-thumb" />
-              <p>Heather</p>
-            </div>
-          </div>
-          <div className="response-inner">
-            <div className="image-thumbnail">
-              <img className="profile-img" src="https://bit.ly/2Pmd5FK" alt="profile-thumb" />
-              <p>Liz</p>
-            </div>
-          </div>
-          <div className="response-inner">
-            <div className="image-thumbnail">
-              <img className="profile-img" src="https://bit.ly/2Pmd5FK" alt="profile-thumb" />
-              <p>Ashley</p>
-            </div>
-          </div>
-          <div className="response-inner">
-            <div className="image-thumbnail">
-              <img className="profile-img" src="https://bit.ly/2Pmd5FK" alt="profile-thumb" />
-              <p>Monique</p>
-            </div>
-          </div>
-          <div className="response-inner">
-            <div className="image-thumbnail">
-              <img className="profile-img" src="https://bit.ly/2Pmd5FK" alt="profile-thumb" />
-              <p>Patrice</p>
-            </div>
-          </div>
+          {uploadVideo()}
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 
