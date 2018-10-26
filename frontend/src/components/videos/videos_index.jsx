ff6712ac9f504d43d1030540e5f5d3e601851405
sirivat <dsirivat17@gmail.com>
2018-10-26 15:47:18 -0700

    Only display filtered videos

--- frontend/src/components/videos/videos_index.jsx
+++ frontend/src/components/videos/videos_index.jsx
@@ -6,7 +6,7 @@ import VideosIndexItem from "./videos_index_item";
 import "./animate.css";
 
 const FIREBASE_VIDEO_URL =
-  "https://firebasestorage.googleapis.com/v0/b/crushd-efd3f.appspot.com/o/userVideo_5bcd152cad51222e4005d4a5.mp4?alt=media&token=d2acb0e3-28d7-4f43-b43e-dfd7bb3c1ae9";
+  "https://firebasestorage.googleapis.com/v0/b/crushd-efd3f.appspot.com/o/userVideo_5bd392cc24e31b0cd2834338.mp4?alt=media&token=d2acb0e3-28d7-4f43-b43e-dfd7bb3c1ae9";
 
 class VideosIndex extends React.Component {
   constructor(props) {
@@ -70,9 +70,19 @@ class VideosIndex extends React.Component {
   }
 
   render() {
+    console.log(this.state.videos);
     return (
       <div className="videos-index-container">
-        <VideosIndexItem firebaseURL={FIREBASE_VIDEO_URL} />
+        {this.state.videos.map(video => {
+          return (
+            <VideosIndexItem
+              key={video.id}
+              firebaseURL={`https://firebasestorage.googleapis.com/v0/b/crushd-efd3f.appspot.com/o/${
+                video.videoURL
+              }?alt=media&token=d2acb0e3-28d7-4f43-b43e-dfd7bb3c1ae9`}
+            />
+          );
+        })}
       </div>
     );
   }
