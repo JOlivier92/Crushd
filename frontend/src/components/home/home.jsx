3b00f51d42a6be863a0be58ac1f9108505ac3ab8
sirivat <dsirivat17@gmail.com>
2018-10-25 19:38:35 -0700

    Fix audio feedback problem

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -58,6 +58,7 @@ class Home extends React.Component {
         this.chunks.push(e.data);
       }
     };
+
     await this.sleep(1500);
     this.setState({ loading: false });
     // Show video recorder to user
@@ -200,7 +201,6 @@ class Home extends React.Component {
     });
   }
 
-
   render() {
     const { recording, videos, recorded, loading } = this.state;
 
@@ -273,6 +273,8 @@ class Home extends React.Component {
 
         <div className="camera">
           <video
+            muted
+            className="camera-view"
             style={{ width: 400 }}
             ref={v => {
               this.video = v;
