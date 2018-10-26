fc5736cfbeed0121699788c4d5b04da12531c8c2
domobritton <domonicbritton@gmail.com>
2018-10-26 11:29:47 -0700

    Add like and dislike buttons

--- frontend/src/components/videos/videos_index.jsx
+++ frontend/src/components/videos/videos_index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Heart from "./like-heart.svg";
+import './videos_index.css';
 
 const FIREBASE_VIDEO_URL =
   "https://firebasestorage.googleapis.com/v0/b/crushd-efd3f.appspot.com/o/userVideo_5bcd152cad51222e4005d4a5.mp4?alt=media&token=d2acb0e3-28d7-4f43-b43e-dfd7bb3c1ae9";
@@ -7,7 +9,7 @@ class VideosIndex extends React.Component {
   render() {
     return (
       <div className="videos-index-view">
-        <h1>This is the videos index section</h1>
+        <h1>Shivani, 24</h1>
         <video
           id="movie"
           src={FIREBASE_VIDEO_URL}
@@ -21,9 +23,22 @@ class VideosIndex extends React.Component {
         >
           {" "}
         </video>
+        <div className="video-view-btns">
+          <div className="btn-outer-one">
+            <div className="btn-inner-one">
+              <i className="fas fa-times" />
+            </div>
+          </div>
+          <div className="btn-outer-two">
+            <div className="btn-inner-two">
+              <img src={Heart} alt="like" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
 
 export default VideosIndex;
