375dfbb9a19ee71454eaa947569115282d9afc0e
sirivat <dsirivat17@gmail.com>
2018-10-27 14:26:45 -0700

    Add login to left and right buttons on videosIndex

--- frontend/src/components/videos/videos_index.jsx
+++ frontend/src/components/videos/videos_index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Heart from "./like-heart.svg";
-import './videos_index.css';
+import "./videos_index.css";
 import Loader from "react-loader-spinner";
 import VideosIndexItem from "./videos_index_item";
 import "./animate.css";
@@ -14,7 +14,7 @@ class VideosIndex extends React.Component {
     this.state = {
       videos: [],
       loading: true
-    }
+    };
 
     this.checkKey = this.checkKey.bind(this);
   }
@@ -24,7 +24,10 @@ class VideosIndex extends React.Component {
     document.onkeydown = this.checkKey;
     await this.props.fetchVideos(this.props.currentUser.id);
     await this.sleep(1000);
-    this.setState({ loading: false });
+    this.setState(
+      { loading: false },
+      () => (this.videoIndexCount = this.props.videos.length)
+    );
   }
 
   sleep(ms) {
@@ -33,23 +36,38 @@ class VideosIndex extends React.Component {
 
   checkKey(e) {
     e = e || window.event;
-
+    console.log(this.videoIndexCount);
     if (e.keyCode == "38") {
       // up arrow
     } else if (e.keyCode == "40") {
       // down arrow
     } else if (e.keyCode == "37") {
+      if (this.videoIndexCount - 1 >= 0) {
+        let video = document.getElementsByClassName("videos-index-view")[
+          this.videoIndexCount - 1
+        ];
+        console.log(video);
+        video.classList.toggle("animated");
+        video.classList.toggle("fadeOutLeft");
+        this.videoIndexCount--;
+      }
       // left arrow
     } else if (e.keyCode == "39") {
       // right arrow
-      let video = document.getElementsByClassName("videos-index-view")[0];
-      video.classList.toggle("animated");
-      video.classList.toggle("fadeOutRight");
+      if (this.videoIndexCount - 1 >= 0) {
+        let video = document.getElementsByClassName("videos-index-view")[
+          this.videoIndexCount - 1
+        ];
+        console.log(video);
+        video.classList.toggle("animated");
+        video.classList.toggle("fadeOutRight");
+        this.videoIndexCount--;
+      }
     }
   }
 
   render() {
-    const {loading} = this.state;
+    const { loading } = this.state;
     if (loading) {
       return (
         <div className="loader-container">
@@ -59,11 +77,20 @@ class VideosIndex extends React.Component {
     }
     return (
       <div className="videos-index-container">
-        <VideosIndexItem firebaseURL={FIREBASE_VIDEO_URL} />
+        {this.props.videos.map(video => (
+          <VideosIndexItem
+            className="video-index-view"
+            key={video.id}
+            firebaseURL={
+              "https://firebasestorage.googleapis.com/v0/b/crushd-efd3f.appspot.com/o/" +
+              video.videoURL +
+              "?alt=media&token=d2acb0e3-28d7-4f43-b43e-dfd7bb3c1ae9"
+            }
+          />
+        ))}
       </div>
     );
   }
 }
-
 
 export default VideosIndex;
