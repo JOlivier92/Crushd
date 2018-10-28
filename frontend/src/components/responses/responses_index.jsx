2fd3a998a27077dc90e4afbaf86833fd4335bdad
Joseph Olivier <josepholivier92@gmail.com>
2018-10-27 18:22:02 -0700

    work on response componenets

--- frontend/src/components/responses/responses_index.jsx
+++ frontend/src/components/responses/responses_index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import ResponseVideosIndexItem from "./responses_index_item";
 
 import "./responses_index.css";
 
@@ -8,7 +9,6 @@ class ResponsesIndex extends React.Component {
     super(props);
     this.state = {
       loading: true,
-      responseVideos: []
     };
   }
 
@@ -18,41 +18,64 @@ class ResponsesIndex extends React.Component {
 
   async componentDidMount() {
     await this.props.fetchResponseVideos(this.props.currentUser.id);
+    debugger;
     await this.sleep(1000);
     this.setState({ loading: false });
   }
+  
 
   render() {
-    const uploadVideo = () => (
-      <div className="upload-video-option-container">
-        <h3>No responses yet...</h3>
-        <div className="upload-inner">
-          <button
-            className="add-btn"
-            onClick={() => this.props.history.push("/upload")}
-          >
-            <i className="fa fa-plus fa-lg" />
-          </button>
-        </div>
-      </div>
-    );
-    return (
-      <div className="responses-index-view">
-        <div className="response-outer">
-          {/* <div className="response-inner">
-            <div className="image-thumbnail">
-              <img
-                className="profile-img"
-                src="https://bit.ly/2Pmd5FK"
-                alt="profile-thumb"
-              />
-              <p>Andrea</p>
+    const uploadVideo = (
+          <div className="upload-video-option-container">
+            <h3>No responses yet...</h3>
+            <div className="upload-inner">
+              <button
+                className="add-btn"
+                onClick={() => this.props.history.push("/upload")}
+              >
+                <i className="fa fa-plus fa-lg" />
+              </button>
             </div>
-          </div> */}
-          {uploadVideo()}
-        </div>
-      </div>
+          </div>
     );
+    debugger;
+    const showMatches = (
+      <div className="upload-video-option-container">
+        <div className="upload-inner">
+          <div className="response-videos-index-container">
+            {Object.values(this.props.responseVideos).map(video => (
+              <ResponseVideosIndexItem
+                className="response-video-index-view"
+                key={video.id}
+                firebaseURL={
+                  "https://firebasestorage.googleapis.com/v0/b/crushd-efd3f.appspot.com/o/" +
+                  video.videoURL +
+                  "?alt=media&token=d2acb0e3-28d7-4f43-b43e-dfd7bb3c1ae9"
+                }
+              />
+            ))}
+          </div>
+        </div>
+    </div>
+    );
+    debugger;
+    if (this.props.responseVideos.length > 0) {
+      return (
+        <div className="responses-index-view">
+          <div className="response-outer">
+            {showMatches}
+          </div>
+        </div>
+      ); 
+    } else {
+      return (
+        <div className="responses-index-view">
+          <div className="response-outer">
+            {uploadVideo}
+          </div>
+        </div>
+      );
+    }  
   }
 }
 
---
+++ frontend/src/components/responses/responses_index_item.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ResponseVideosIndexItem = ({ firebaseURL }) => {
+    return <div className="response-videos-index-view">
+        <h1>Your contenders</h1>
+        <video id="movie" src={firebaseURL} height="" width="80%" autoPlay controls muted loop play="true">
+          {" "}
+        </video>
+        <div className="video-view-btns">
+          <div className="btn-outer-one">
+            <div className="btn-inner-one">
+              <i className="fas fa-times" />
+            </div>
+          </div>
+          <div className="btn-outer-two">
+            <div className="btn-inner-two">
+              <i className="fas fa-times" />
+            </div>
+          </div>
+        </div>
+      </div>;
+};
+
+export default ResponseVideosIndexItem;
